3024a4b8eef5a3ae3b9218ff7c307ce0e3c0dd70
ArianaVillegas <ariana.villegas@utec.edu.pe>
2020-06-26 14:32:52 -0500

    Update KNNsearch

--- Algortimos/KNNsearch.py
+++ Algortimos/KNNsearch.py
@@ -5,33 +5,27 @@ from scipy.spatial import distance
 
 def KNNsearch(query, k, metric):
     result = []
-    unique_names = list(set(Collection["names"]))
-    if k >= len(unique_names):
-        return unique_names
-    for i in range(len(unique_names)):
-        start = Collection["names"].index(unique_names[i])
+    for i in range(len(Collection["names"])):
         dist = math.inf
-        while start<len(Collection["names"]) and Collection["names"][start] == unique_names[i]:
-            if(metric=="eucledian"):
-                ndist = distance.euclidean(query, Collection["encodings"][i])
-            elif(metric=="manhattan"):
-                ndist = distance.cityblock(query, Collection["encodings"][i])
-            dist = min(dist,ndist)
-            start += 1
+        if(metric=="eucledian"):
+            ndist = distance.euclidean(query, Collection["encodings"][i])
+        elif(metric=="manhattan"):
+            ndist = distance.cityblock(query, Collection["encodings"][i])
+        dist = min(dist,ndist)
         if(len(result)<k):
-            heapq.heappush(result,(-dist,unique_names[i]))
+            heapq.heappush(result,(-dist,Collection["names"][i]))
         elif(heapq.nsmallest(1,result)[0][0]<-dist):
-            heapq.heapreplace(result,(-dist,unique_names[i]))
-    return [pp[1] for pp in sorted(result)][::-1]
+            heapq.heapreplace(result,(-dist,Collection["names"][i]))
+    return result
 
 
-path_name = "00000002.jpg"
+path_name = "Abdullah_Gul_0008.jpg"
 metric = ["eucledian", "manhattan"]
 
 imagePath = os.path.relpath(path_name)
 print(imagePath)
 query = getFeatures(imagePath)
 
-ans = KNNsearch(query,4,metric[1])
+ans = KNNsearch(query,20,metric[0])
 for i in ans:
     print(i)
